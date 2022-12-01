c5eba22c51acc54f7d42eea297db1502d66b9f6b
br19 <benjamin.raible@uni-ulm.de>
2022-12-01 19:55:54 +0100

    Implement movie_list_spider.py

---
+++ src/movies/movies/spiders/movie_list_spider.py
@@ -0,0 +1,61 @@
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from globals import MOVIE_LIST_SPIDER
+from movies.URLs import URLS
+
+
+class MovieListSpider(scrapy.Spider):
+    """
+    Spider responsible for storing movies to be further scraped
+    """
+    name = "MOVIE_LIST_SPIDER"
+    year_list_extractor = LinkExtractor(allow=[r"/wiki/List_of_American_films_of_\d+"])
+    download_delay = 5
+
+    def start_requests(self):
+        url_start = 'https://en.wikipedia.org/wiki/'
+        url_end = '_in_film'
+        for year in range(1950, 2023):
+            url = url_start + str(year) + url_end
+            yield scrapy.Request(url=url, callback=self.parse_year, cb_kwargs=dict(year=year))
+
+    def parse_year(self, response, year):
+        # TODO extract only valid links
+        link_list = response.selector.xpath(
+            '//div[@class="mw-parser-output"]/ul[preceding::h2[./span[@id="2022_films"]]]/li/a[contains(text(), "List of")]/@href').getall()
+        if len(link_list) > 0:  # true for the later years
+            link_prefix = 'https://en.wikipedia.org'
+            for link in link_list:
+                yield scrapy.Request(link_prefix + link, callback=self.parse_film_list, cb_kwargs=dict(year=year))
+
+    def parse_film_list(self, response, year):
+        # look for "Notable films released in" section and get links
+        link_list = response.selector.xpath(
+            '//ul[preceding::h2[./span[@id="Notable_films_released_in_1990"]] and following-sibling::h2[./span['
+            '@id="Deaths"]]]/li/i/a/@href').getall()
+
+        # TODO if not prev. look for https://en.wikipedia.org/wiki/List_of_Tamil_films_of_2005
+        if len(link_list) == 0:
+            link_list = response.selector.xpath(
+                '//table[preceding-sibling::h2[./span[contains(@id,"List_of_")]]]/tbody/tr[preceding-sibling::tr/th['
+                'contains(text(),"Opening")]]/td/i/a/@href').getall()
+
+        # TODO if not prev. look for tabel with Title in header th column and link (new format)
+        if len(link_list) == 0:
+            link_list = response.selector.xpath(
+                '//table/tbody/tr[preceding-sibling::tr/th[contains(text(),"Opening")]]/td/i/a/@href').getall()
+
+        # TODO if not prev. look for https: // en.wikipedia.org / wiki / List_of_French_films_of_2005
+
+        if len(link_list) == 0:
+            link_list = response.selector.xpath(
+                '//table/tbody[tr/th[contains(text(),"Title")]]/tr/td/i/a/@href').getall()
+
+        # TODO if not prev. print url for checking what went wrong
+        if len(link_list) == 0:
+            print("Fail:", response.url)
+
+        for link in link_list:
+            yield {"link": link, "year": year}
+
+# TODO check resulting list for duplicates
