9575c99cdd5bb8733c95e01e5fc2065336545aad
br19 <benjamin.raible@uni-ulm.de>
2022-12-02 01:04:45 +0100

    Crawl for film_list first try

--- src/movies/movies/spiders/movie_indexer.py
+++ src/movies/movies/spiders/movie_indexer.py
@@ -1,6 +1,6 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
-from globals import MOVIE_LIST_SPIDER
+# from globals import MOVIE_LIST_SPIDER
 from movies.URLs import URLS
 
 
@@ -8,7 +8,7 @@ class WikiMovieIndexSpider(scrapy.Spider):
     """
     Spider responsible for storing movies to be further scraped
     """
-    name = MOVIE_LIST_SPIDER
+   # name = MOVIE_LIST_SPIDER
     year_list_extractor = LinkExtractor(allow=[r"/wiki/List_of_American_films_of_\d+"])
 
     def start_requests(self):
